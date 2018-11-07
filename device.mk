8f7b6a54c9911f6409d1ab410e85cb621fb4c728
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    whyred: Enforce RRO for all resource overlays
    This configuration will likely go in mainline_system configuration.
    
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1
    Signed-off-by: fakeyatogod <fakeyatokami@gmail.com>

--- device.mk
+++ device.mk
@@ -31,6 +31,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += device/xiaomi/whyred
 
