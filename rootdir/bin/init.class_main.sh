c0c592c70fd9ba5c353ec8af92cc581f6c78713f
Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>
2019-02-28 12:27:33 -0700

    init.qcom.rc: FR 53051: remove configs for vendor data modules
    As part of this FR we are moving vendor data specific module
    configurations to custom vendor data init rc files
    
    Change-Id: I64e839c2b0fbd0826fe79b95259af2a10e8b1ea1
    Signed-off-by: fakeyatogod <fakeyatokami@gmail.com>

--- rootdir/bin/init.class_main.sh
+++ rootdir/bin/init.class_main.sh
@@ -1,6 +1,6 @@
 #! /vendor/bin/sh
 
-# Copyright (c) 2013-2018, The Linux Foundation. All rights reserved.
+# Copyright (c) 2013-2014, 2019 The Linux Foundation. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -122,15 +122,11 @@ case "$baseband" in
     case "$datamode" in
         "tethered")
             start vendor.dataqti
-            start vendor.port-bridge
             ;;
         "concurrent")
             start vendor.dataqti
-            start vendor.netmgrd
-            start vendor.port-bridge
             ;;
         *)
-            start vendor.netmgrd
             ;;
     esac
 esac
