d3fcec55c225ad727d4cc01ece32c5b862cf151b
dianlujitao <dianlujitao@lineageos.org>
2019-06-01 14:54:11 +0800

    whyred: Implement a script for goodix data migration
    Change-Id: I171bbb194594cbc8ef88f63c5b7afa323d5aed16
    Signed-off-by: Eric Farias <erikofarias@gmail.com>

--- device.mk
+++ device.mk
@@ -196,7 +196,9 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint feature
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_whyred
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_whyred \
+    move_goodix_data.sh
+
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
