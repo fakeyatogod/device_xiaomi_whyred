7d1924782256099c4071bf88693c95a34b323fe6
Robert Shih <robertshih@google.com>
2019-11-25 22:25:42 -0800

    whyred: Enable v1.3 clearkey drm plugin
    Bug: 139134043
    Test: MediaDrmTest
    Change-Id: Ie34e59947577988f75fae52d22e3f573f4b92420
    Signed-off-by: Arian <arian.kulmer@web.de>

--- device.mk
+++ device.mk
@@ -224,7 +224,7 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 # Fingerprint feature
 PRODUCT_PACKAGES += \
