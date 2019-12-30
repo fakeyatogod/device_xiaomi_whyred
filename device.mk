9e16fcd3f3c45d160f29e6ea7ae886765f6c3368
srfarias <erikofarias@gmail.com>
2019-12-30 23:43:13 +0000

    Revert "whyred: build system thermal hidl 1.1"
    This reverts commit ee27ef0369bb016d5b1f585945ea991212727320.

--- device.mk
+++ device.mk
@@ -434,7 +434,6 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.1 \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service \
     thermal.sdm660
