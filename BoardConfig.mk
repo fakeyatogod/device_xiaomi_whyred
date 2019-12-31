f5617ab2679db78210a21b3ae0d7d8a0c57ecc3f
FacuM <facumo.fm@gmail.com>
2019-12-31 06:25:30 +0000

    BoardConfig: switch to GNU GCC 10.0 as our kernel toolchain.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,6 +52,10 @@ TARGET_2ND_CPU_VARIANT := cortex-a73
 
 TARGET_USES_64_BIT_BINDER := true
 
+# Kernel Toolchain
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-gnueabi-10.0/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-gnu-
+
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3 androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += loop.max_part=7
