a33b4878efedf025377264d8bf7138212fb839c2
Subhajeet Muhuri <kenny3fcb@gmail.com>
2020-01-21 14:37:22 +0530

    whyred: Import sdm660 adjusted powerhint.json

--- device.mk
+++ device.mk
@@ -343,6 +343,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3-service.pixel-libperfmgr
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
