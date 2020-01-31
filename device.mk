aef99199c4dd043676e05bce56817df82b7e2967
Jaideep Sharma <jaidshar@codeaurora.org>
2020-02-01 00:04:22 +0530

    whyred: update audio hal,effect hidl version to 6.0
    Change-Id: Ia8b355884b2c3aa00fa2c4d12ef233bd4dfc67bf

--- device.mk
+++ device.mk
@@ -103,9 +103,9 @@ PRODUCT_BOOT_JARS += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@5.0-impl \
+    android.hardware.audio@6.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
