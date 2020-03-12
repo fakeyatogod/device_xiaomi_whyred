4f9a82cf006075bcc6f5625a33f0120d7bc8d11f
fakeyatogod <fakeyatokami@gmail.com>
2020-03-12 20:27:50 +0530

    whyred: No Snap For Now

--- device.mk
+++ device.mk
@@ -152,7 +152,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     libxml2 \
-    Snap \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # CNE
