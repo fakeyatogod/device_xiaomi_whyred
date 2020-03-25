7b37607a1f20542205f1ee282bda67d2936da2e2
Shashank Baghel <theradcolor@gmail.com>
2020-03-25 12:55:59 +0530

    whyred: rootdir: let the kernel set the swappiness
    swappiness will be set to 60% from the kernel unless it's modified from there
    
    Signed-off-by: Shashank Baghel <theradcolor@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -352,7 +352,6 @@ if [ "$ProductName" == "msmnile" ] || [ "$ProductName" == "kona" ] || [ "$Produc
       configure_zram_parameters
       # configure_read_ahead_kb_values
       echo 0 > /proc/sys/vm/page-cluster
-      echo 100 > /proc/sys/vm/swappiness
 else
     arch_type=`uname -m`
     MemTotalStr=`cat /proc/meminfo | grep MemTotal`
@@ -449,9 +448,7 @@ else
     fi
 
     # Set allocstall_threshold to 0 for all targets.
-    # Set swappiness to 100 for all targets
     echo 0 > /sys/module/vmpressure/parameters/allocstall_threshold
-    echo 100 > /proc/sys/vm/swappiness
 
     configure_zram_parameters
 
@@ -3663,7 +3660,6 @@ case "$target" in
 
             # Turn on sleep modes.
             echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
-            echo 100 > /proc/sys/vm/swappiness
             ;;
         esac
     ;;
@@ -4218,7 +4214,6 @@ case "$target" in
 	echo N > /sys/module/lpm_levels/L3/l3-dyn-ret/idle_enabled
         # Turn on sleep modes.
         echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
-	echo 100 > /proc/sys/vm/swappiness
 	echo 120 > /proc/sys/vm/watermark_scale_factor
     ;;
 esac
