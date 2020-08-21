ea6a357892a53403de5746a2f106af9757a9e36f
Weilun Du <wdu@google.com>
2020-08-21 14:45:14 -0700

    whyred: Add TetheringOverlay
    BUG: 163800715
    Signed-off-by: Weilun Du <wdu@google.com>
    Change-Id: Ia0834f5215ff8c3b13effa591bd2f84d93419a06

--- device.mk
+++ device.mk
@@ -503,7 +503,8 @@ PRODUCT_PACKAGES += \
     wificond \
     wpa_supplicant \
     wpa_supplicant.conf \
-    WifiOverlay
+    WifiOverlay \
+    TetheringConfigOverlay
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
