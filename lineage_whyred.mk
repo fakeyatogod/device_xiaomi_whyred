c83f2323913f2ee8464817dc2e00365732f0b7fc
fakeyatogod <fakeyatokami@gmail.com>
2020-10-08 14:15:29 +0000

    whyred: Lets build lineage

--- lineage_whyred.mk
+++ lineage_whyred.mk
@@ -29,12 +29,9 @@ $(call inherit-product, device/xiaomi/whyred/device.mk)
 $(call inherit-product, vendor/MiuiCamera/config.mk)
 
 # Inherit Carbon product configuration
-$(call inherit-product, vendor/carbon/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit Carbon GSM telephony parts
-$(call inherit-product, vendor/carbon/config/gsm.mk)
-
-PRODUCT_NAME := carbon_whyred
+PRODUCT_NAME := lineage_whyred
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
