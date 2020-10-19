11a8f4337d7ac2d62d4cb5cc3007fb20dbe8f686
fakeyatogod <fakeyatokami@gmail.com>
2020-10-19 15:55:50 +0530

    whyred: Ignore neverallows until they are addressed

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -215,6 +215,7 @@ include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 BOARD_VNDK_RUNTIME_DISABLE := true
